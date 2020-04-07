62fa7db87249356a8c760b25ef9a0468639be613
renzo-chavez <renzomartinch@gmail.com>
2020-04-07 10:47:58 -0500

    vf

--- librerias.py
+++
@@ -1,6 +0,0 @@
-#Se impoortan la librerías necesarias
-import numpy as np
-import pandas as pd
-import itertools as it
-import matplotlib.pyplot as plt
-from sklearn.metrics import mean_absolute_error
