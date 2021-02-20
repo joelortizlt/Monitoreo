c9b5c4688007dc941aeee07bab2d26a7cba717a5
renzo-chavez <renzomartinch@gmail.com>
2021-02-19 21:41:18 -0500

    final

--- ExtraccionNoRev.py
+++ ExtraccionNoRev.py
@@ -8,25 +8,25 @@ from source.engine.InputsNoRevolvente import InputsNoRevolvente
 from source.engine.OutputsNoRevolvente import OutputsNoRevolvente
 
 #CAMBIAR
-nombreproducto = 'estacional'
-inicio = 201812
-fin = 201912
+nombreproducto = 'cef'
+inicio = 201901
+fin = 202012
 
-
-
-#agregado_cortes=['C_SEGMENTO','C_MONEDA','C_PLAZO','C_OK']
-#agregado_cortes=['C_SEGMENTO','C_MONEDA','C_PLAZO','C_CANAL','C_OK']
+#agregado_cortes=['C_SEGMENTO','C_MONEDA','C_PLAZO','C_OK']                     # Gahi & Veh
+#agregado_cortes=['C_SEGMENTO','C_PLAZO','C_OK']                                # Hipot & MiViv
+agregado_cortes=['C_SEGMENTO','C_MONEDA','C_PLAZO','C_CANAL','C_OK']            # CEF
 #agregado_cortes=['C_PRODUCTO','C_MONEDA','C_PYG']
-agregado_cortes=['C_CAMPANIA','C_PLAZO','C_OK']
+#agregado_cortes=['C_CAMPANIA','C_PLAZO','C_OK']
 
-#lista_cortes=[['C_SEGMENTO'],['C_MONEDA'],['C_PLAZO'],['C_OK']]
-#lista_cortes=[['C_SEGMENTO'],['C_MONEDA'],['C_PLAZO'],['C_CANAL'],['C_OK']]
+#lista_cortes=[['C_SEGMENTO'],['C_MONEDA'],['C_PLAZO'],['C_OK']]                # Gahi & Veh
+#agregado_cortes=['C_SEGMENTO','C_PLAZO','C_OK']                                # Hipot & MiViv
+lista_cortes=[['C_SEGMENTO'],['C_MONEDA'],['C_PLAZO'],['C_CANAL'],['C_OK']]     # CEF
 #lista_cortes=[['C_PRODUCTO'],['C_MONEDA'],['C_PYG']]
-lista_cortes=[['C_CAMPANIA'],['C_PLAZO'],['C_OK']]
+#lista_cortes=[['C_CAMPANIA'],['C_PLAZO'],['C_OK']]
 
-ruta_real=['/Users/renzomartinch/Downloads/ComiteSetiembre/'+str(nombreproducto)+'_reales.csv']
-ruta_teorico=['/Users/renzomartinch/Downloads/ComiteSetiembre/'+str(nombreproducto)+'_inputs.csv']
-ruta_tmin=['/Users/renzomartinch/Downloads/ComiteSetiembre/'+str(nombreproducto)+'_precios.csv']
+ruta_real=['/Users/renzomartinch/Downloads/Comite/'+str(nombreproducto)+'_reales.csv']
+ruta_teorico=['/Users/renzomartinch/Downloads/Comite/'+str(nombreproducto)+'_inputs.csv']
+ruta_tmin=['/Users/renzomartinch/Downloads/Comite/'+str(nombreproducto)+'_precios.csv']
 
 n = len(agregado_cortes)
 
@@ -35,23 +35,28 @@ TEORICO = pd.read_csv(ruta_teorico[0])#, encoding='latin-1')
 TMIN = pd.read_csv(ruta_tmin[0])#, encoding='latin-1')
     
 product = InputsNoRevolvente(REAL,TEORICO,mincosecha=inicio,maxcosecha=fin)
+
+#Inputs
 product.condensar(agregado_cortes)
 product.optimizar()
-product.impactoTmin(TMIN)
-#product.impactoTIR(TMIN)
-
 a = product.promedios
 b = product.stats.drop(product.stats.iloc[:, 0:(n+1)], axis = 1)
+
+#Tmin
+product.impactoTmin(TMIN)
 c = product.Tmin.drop(product.Tmin.iloc[:, 0:(n+1)], axis = 1) 
-#d = product.TIR.drop(product.TIR.iloc[:, 0:(n+1)], axis = 1)
 
-#product = OutputsNoRevolvente(REAL,TEORICO,mincosecha=inicio,maxcosecha=fin)
-#product.condensar(agregado_cortes)
+#TIR
+product.impactoTIR(TMIN)
+d = product.TIR.drop(product.TIR.iloc[:, 0:(n+1)], axis = 1)
 
-#e = product.ratios.drop(product.ratios.iloc[:, 0:(n+2)], axis = 1)
-#f = product.niveles.drop(product.niveles.iloc[:, 0:(n+2)], axis = 1)
+#Outputs
+product = OutputsNoRevolvente(REAL,TEORICO,mincosecha=inicio,maxcosecha=fin)
+product.condensar(agregado_cortes)
+e = product.ratios.drop(product.ratios.iloc[:, 0:(n+2)], axis = 1)
+f = product.niveles.drop(product.niveles.iloc[:, 0:(n+2)], axis = 1)
 
-agregado = pd.concat([a,b,c], axis=1) #<- añadir d,e,f
+agregado = pd.concat([a,b,c,d,e,f], axis=1) #<- añadir c,d,e,f
 
 first = True
 for corte in lista_cortes:
@@ -63,8 +68,8 @@ for corte in lista_cortes:
         temp = agregado.loc[agregado[corte[0]] == condensado.loc[j,corte[0]]]
         r = temp['recuento']
         m = temp['Monto']
-        #e = temp['Capital promedio']
-        #s = temp['n_saldo_real']
+        e = temp['Capital promedio']
+        s = temp['n_saldo_real']
 
         condensado.at[j,'recuento'] = sum(r)
         for k in ['pd_real','can_real','pre_real','pd_teorico','can_teorico','pre_teorico','pd_optimo','can_optimo','pre_optimo','MAE_pd','MAE_can','MAE_pre','MAEop_pd','MAEop_can','MAEop_pre','scalar_pd','scalar_can','scalar_pre']:
@@ -74,15 +79,15 @@ for corte in lista_cortes:
             condensado.at[j,k] = sum(temp[k] * m) / sum(m)
         condensado.at[j,'Monto'] = sum(m)
             
-        #for k in ['TIR_base','delta_TIR_pd','delta_TIR_can','delta_TIR_pre','TIR_final']:
-        #    condensado.at[j,k] = sum(temp[k] * e) / sum(e)
-        #condensado.at[j,'Capital promedio'] = sum(e)
+        for k in ['TIR_base','delta_TIR_pd','delta_TIR_can','delta_TIR_pre','TIR_final']:
+            condensado.at[j,k] = sum(temp[k] * e) / sum(e)
+        condensado.at[j,'Capital promedio'] = sum(e)
 
-        #for k in ['r_if_real','r_ef_real','r_spread_bruto_real','r_if_teorico','r_ef_teorico','r_spread_bruto_teorico']:
-        #    condensado.at[j,k] = sum(temp[k] * s) / sum(s)
+        for k in ['r_if_real','r_ef_real','r_spread_bruto_real','r_if_teorico','r_ef_teorico','r_spread_bruto_teorico']:
+            condensado.at[j,k] = sum(temp[k] * s) / sum(s)
 
-        #for k in ['n_if_real','n_ef_real','n_saldo_real','n_if_teorico','n_ef_teorico','n_saldo_teorico']:
-        #    condensado.at[j,k] = sum(temp[k])
+        for k in ['n_if_real','n_ef_real','n_saldo_real','n_if_teorico','n_ef_teorico','n_saldo_teorico']:
+            condensado.at[j,k] = sum(temp[k])
 
     nametemp=condensado.columns[0]
     condensado.rename(columns={nametemp:"CORTE"}, inplace=True)
